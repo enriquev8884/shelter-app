3ec5292b18c303daf658084dc1b247b41cbc6258
Jenny Feng <jennyfeng.ga.ling1999@gmail.com>
2017-03-15 17:16:58 +0000

    change env for pet key and pet secret

--- models/model.rb
+++ models/model.rb
@@ -1,8 +1,8 @@
 # https://github.com/ehutzelman/petfinder
 require 'petfinder'
   Petfinder.configure do |config|
-    config.api_key = "472d66ad2bc7f83e735c4552db634039"
-    config.api_secret = "0a50c24a7ed5982100eed6d203ad37b1"
+    config.api_key = ENV["PET_KEY"]
+    config.api_secret = ENV["PET_SECRET"]
   end
   @@petfinder = Petfinder::Client.new
   
